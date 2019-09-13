5bb62dcac555ec5f8d481ffa00faf3d0ce27f344
dsanjana20@gmail.com <19961120@dsH>
2019-09-13 21:42:53 +0530

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/**
+ *
+ * @author ASUS
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+
+
+    public static void main(String[] args){
+        final String regex = "((?:(?:for|if|while)))\\s*\\s*\\(.*?\\)\\s*(\\{(?:\\{[^\\{}]*\\}|.)*?\\})";
+        final String string = "while(i<4){\n"
+                + "	 if(){\n"
+                + "	}\n"
+                + "}";
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
+        final Matcher matcher = pattern.matcher(string);
+
+        if (matcher.find()) {
+            System.out.println("Full match: " + matcher.group(0));
+            for (int i = 1; i <= matcher.groupCount(); i++) {
+                System.out.println("Group " + i + ": " + matcher.group(i));
+            }
+        }
+
+    }
+}
